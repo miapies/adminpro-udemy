daaa3be7713a500fcde7b88d56761d566624574e
miapies@gmail.com <miapies78@gmail.com>
2018-08-14 16:12:17 +0200

    Despliegue a Heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,3 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://backend-adminpro.herokuapp.com';
 export const LIMIT_GET = 4;
